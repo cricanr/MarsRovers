27d5329dcc7ed20c65f7da1e4e1bd61a83a638f6
Radu Crican <radu.crican@workday.com>
2018-05-28 09:43:38 +0200

    added test cases

--- src/test/scala/mars/rovers/RoverSpec.scala
+++ src/test/scala/mars/rovers/RoverSpec.scala
@@ -16,11 +16,21 @@ class RoverSpec extends WordSpec with Matchers {
       }
     }
 
-    "calling move method" should {
-      "return new position" in {
+    "calling move method for a given drone 1: Rover(Coordinates(1, 2), N) with movements: Command(Seq(L, M, L, M, L, M, L, M, M))" should {
+      "return new position: Rover(Coordinates(1, 3), N)" in {
         val initialRover = Rover(Coordinates(1, 2), N)
         val expectedRover = Rover(Coordinates(1, 3), N)
         val destinationRover = initialRover.move(Command(Seq(L, M, L, M, L, M, L, M, M)))
+
+        destinationRover shouldEqual expectedRover
+      }
+    }
+
+    "calling move method for a given drone 2: Rover(Coordinates(3, 3), E) with movements: Command(Seq(M, M, R, M, M, R, M, R, R, M))" should {
+      "return new position" in {
+        val initialRover = Rover(Coordinates(3, 3), E)
+        val expectedRover = Rover(Coordinates(5, 1), E)
+        val destinationRover = initialRover.move(Command(Seq(M, M, R, M, M, R, M, R, R, M)))
 
         destinationRover shouldEqual expectedRover
       }
@@ -30,6 +40,36 @@ class RoverSpec extends WordSpec with Matchers {
       "increment y axis position" in {
         val initialRover = Rover(Coordinates(1, 2), N)
         val expectedRover = Rover(Coordinates(1, 3), N)
+        val destinationRover = initialRover.move(Command(Seq(M)))
+
+        destinationRover shouldEqual expectedRover
+      }
+    }
+
+    "calling move when South oriented" should {
+      "decrement y axis position" in {
+        val initialRover = Rover(Coordinates(1, 2), S)
+        val expectedRover = Rover(Coordinates(1, 1), S)
+        val destinationRover = initialRover.move(Command(Seq(M)))
+
+        destinationRover shouldEqual expectedRover
+      }
+    }
+
+    "calling move when West oriented" should {
+      "decrement x axis position" in {
+        val initialRover = Rover(Coordinates(1, 2), W)
+        val expectedRover = Rover(Coordinates(0, 2), W)
+        val destinationRover = initialRover.move(Command(Seq(M)))
+
+        destinationRover shouldEqual expectedRover
+      }
+    }
+
+    "calling move when East oriented" should {
+      "increment x axis position" in {
+        val initialRover = Rover(Coordinates(1, 2), E)
+        val expectedRover = Rover(Coordinates(2, 2), E)
         val destinationRover = initialRover.move(Command(Seq(M)))
 
         destinationRover shouldEqual expectedRover
@@ -46,10 +86,70 @@ class RoverSpec extends WordSpec with Matchers {
       }
     }
 
+    "calling move left when South oriented" should {
+      "change orientation to West" in {
+        val initialRover = Rover(Coordinates(1, 2), S)
+        val expectedRover = Rover(Coordinates(1, 2), E)
+        val destinationRover = initialRover.move(Command(Seq(L)))
+
+        destinationRover shouldEqual expectedRover
+      }
+    }
+
+    "calling move left when West oriented" should {
+      "change orientation to South" in {
+        val initialRover = Rover(Coordinates(1, 2), W)
+        val expectedRover = Rover(Coordinates(1, 2), S)
+        val destinationRover = initialRover.move(Command(Seq(L)))
+
+        destinationRover shouldEqual expectedRover
+      }
+    }
+
+    "calling move left when East oriented" should {
+      "change orientation to Noth" in {
+        val initialRover = Rover(Coordinates(1, 2), E)
+        val expectedRover = Rover(Coordinates(1, 2), N)
+        val destinationRover = initialRover.move(Command(Seq(L)))
+
+        destinationRover shouldEqual expectedRover
+      }
+    }
+
     "calling move right when North oriented" should {
       "change orientation to East" in {
         val initialRover = Rover(Coordinates(1, 2), N)
         val expectedRover = Rover(Coordinates(1, 2), E)
+        val destinationRover = initialRover.move(Command(Seq(R)))
+
+        destinationRover shouldEqual expectedRover
+      }
+    }
+
+    "calling move right when South oriented" should {
+      "change orientation to West" in {
+        val initialRover = Rover(Coordinates(1, 2), S)
+        val expectedRover = Rover(Coordinates(1, 2), W)
+        val destinationRover = initialRover.move(Command(Seq(R)))
+
+        destinationRover shouldEqual expectedRover
+      }
+    }
+
+    "calling move right when West oriented" should {
+      "change orientation to North" in {
+        val initialRover = Rover(Coordinates(1, 2), W)
+        val expectedRover = Rover(Coordinates(1, 2), N)
+        val destinationRover = initialRover.move(Command(Seq(R)))
+
+        destinationRover shouldEqual expectedRover
+      }
+    }
+
+    "calling move right when East oriented" should {
+      "change orientation to East" in {
+        val initialRover = Rover(Coordinates(1, 2), E)
+        val expectedRover = Rover(Coordinates(1, 2), S)
         val destinationRover = initialRover.move(Command(Seq(R)))
 
         destinationRover shouldEqual expectedRover
